3f6607711d2a3b1bafd5412fd4b9c136aacefaa3
lordeadroid <rishabhrawat34@gmail.com>
2024-06-13 12:13:33 +0530

    created basic ui for signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import "./App.css";
 
 const App = () => {
-  return <h1>Signup Page</h1>;
+  return (
+    <div className="page">
+      <div className="logo">
+        <h1>antstack</h1>
+      </div>
+      <form className="form">
+        <div className="input-container">
+          <input
+            type="text"
+            name="username"
+            className="input-box"
+            placeholder="username"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            className="input-box"
+            placeholder="email"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            className="input-box"
+            placeholder="password"
+            required
+          />
+        </div>
+        <input type="submit" value="Submit" className="button" />
+      </form>
+    </div>
+  );
 };
 
 export default App;
